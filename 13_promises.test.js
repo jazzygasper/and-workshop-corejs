995ce22eeb19b1689beff99d6127da80fbd9d1c2
Jazzy Gasper <Jasmina.gasper@gmail.com>
2017-11-01 10:35:26 +0000

    promises exercise

--- 13_promises.test.js
+++ 13_promises.test.js
@@ -1,17 +1,15 @@
-/* 
-    
+/*
+
     Task 1) Refactor the anAsyncFunction to use shorthand object promise notation
 
 */
 
 const anAsyncFunction = result => {
-  return new Promise((resolve, reject) => {
     if (result) {
-      return resolve([0, 1, 2]);
+      return Promise.resolve([0, 1, 2]);
     } else {
-      return reject(" Oh no :( ");
+      return Promise.reject(" Oh no :( ");
     }
-  });
 };
 
 test("When resolved, the then is called", done => {
@@ -28,31 +26,46 @@ test("When rejected, the catch handler is called", done => {
   });
 });
 
-/* 
-    
+/*
+
     Task 2) Abstract out the .then calls into a separate function, not anonymous
 
-    Think about: 
+    Think about:
         - What happens if you call .then without returning anything?
         - Does a .then have to return a promise? Can it? Try wrapping addOne's function with: Promise.resolve()
 
 */
 
 const addOne = result => result + 1;
+const addTwo = result => result + 2
+
+const addUpToSix = result => {
+    return Promise.resolve(0)
+        .then(addTwo)
+        .then(addTwo)
+        .then(addTwo);
+}
+
+// test("Add up to 6!", done => {
+//   return Promise.resolve(0)
+//     .then(addTwo)
+//     .then(addTwo)
+//     .then(addTwo)
+//     .then(function(result) {
+//       expect(result).toBe(6);
+//     })
+//     .then(done);
+// });
 
 test("Add up to 6!", done => {
-  return Promise.resolve(0)
-    .then(num => num + 2)
-    .then(num => num + 2)
-    .then(num => num + 2)
-    .then(function(result) {
+  addUpToSix(0).then(result => {
       expect(result).toBe(6);
-    })
-    .then(done);
+    done();
+  });
 });
 
-/* 
-    
+/*
+
     Task 3) Refactor to be flat like the one before
 
 */
@@ -84,7 +97,7 @@ test("A big old chain of promises", done => {
     .then(done);
 });
 
-/* 
+/*
 
     Task 4) Rewrite to use Promise.all
 
@@ -94,53 +107,53 @@ test("A big old chain of promises", done => {
 
 */
 
-const getUser = result => {
-  return Promise.resolve({
-    name: "Alice"
-  });
-};
+// const getUser = result => {
+//   return Promise.resolve({
+//     name: "Alice"
+//   });
+// };
+//
+// const getPlans = result => {
+//   return Promise.resolve({
+//     id: 4,
+//     plan: "My plan"
+//   });
+// };
+//
+// const subscribe = (userName, id) => {
+//   return `Create the plan called ${userName} for plan ID ${id}`;
+// };
+//
+// test("Get Alices plan", done => {
+//   return getUser()
+//     .then(user => {
+//       return getPlans().then(plans => {
+//         return subscribe(user.name, plans.id);
+//       });
+//     })
+//     .then(string => {
+//       expect(string).toBe("Create the plan called Alice for plan ID 4");
+//       done();
+//     });
+// });
 
-const getPlans = result => {
-  return Promise.resolve({
-    id: 4,
-    plan: "My plan"
-  });
-};
+/*
 
-const subscribe = (userName, id) => {
-  return `Create the plan called ${userName} for plan ID ${id}`;
-};
-
-test("Get Alices plan", done => {
-  return getUser()
-    .then(user => {
-      return getPlans().then(plans => {
-        return subscribe(user.name, plans.id);
-      });
-    })
-    .then(string => {
-      expect(string).toBe("Create the plan called Alice for plan ID 4");
-      done();
-    });
-});
-
-/* 
-
-    Task 5) Promise.all returns an array, but that means we have to access with the gross array[0] syntax. 
+    Task 5) Promise.all returns an array, but that means we have to access with the gross array[0] syntax.
         Rewrite the .then to use the spread operator instead
 */
 
-const hey = (firstName, lastName) => {
-  return `Hey ${firstName} ${lastName}!`;
-};
-
-test("Get Alices plan with spread operator", done => {
-  return Promise.all([Promise.resolve("Bob"), Promise.resolve("Miggins")])
-    .then(data => {
-      return hey(data[0], data[1]);
-    })
-    .then(string => {
-      expect(string).toBe("Hey Bob Miggins!");
-      done();
-    });
-});
+// const hey = (firstName, lastName) => {
+//   return `Hey ${firstName} ${lastName}!`;
+// };
+//
+// test("Get Alices plan with spread operator", done => {
+//   return Promise.all([Promise.resolve("Bob"), Promise.resolve("Miggins")])
+//     .then(data => {
+//       return hey(data[0], data[1]);
+//     })
+//     .then(string => {
+//       expect(string).toBe("Hey Bob Miggins!");
+//       done();
+//     });
+// });
